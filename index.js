00dddb8b37ef53f9f56f017cdf11c1847ba31bed
David Bujosa <davidbujosa@gmail.com>
2022-03-12 16:05:58 +0100

    feat: add env, prettier and new packages

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN);
